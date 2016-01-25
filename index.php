4225748038a034ba269dc2c51a385a01c0ce34af
Victor <aston_victor@i.ua>
2016-01-25 13:52:18 +0200

    stash

--- index.php
+++ index.php
@@ -19,5 +19,4 @@ define('DRUPAL_ROOT', getcwd());
 require_once DRUPAL_ROOT . '/includes/bootstrap.inc';
 drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
 menu_execute_active_handler();
-line 2
-new conflict
+stash
