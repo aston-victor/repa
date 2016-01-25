ce4fe5fc33cd3b2d41b883faaefaf574abcdc432
Victor <aston_victor@i.ua>
2016-01-25 13:45:05 +0200

    ready for conflict

--- index.php
+++ index.php
@@ -19,5 +19,5 @@ define('DRUPAL_ROOT', getcwd());
 require_once DRUPAL_ROOT . '/includes/bootstrap.inc';
 drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
 menu_execute_active_handler();
-line
+line 2
 conflict
